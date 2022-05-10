734202b0f8ad30ebbc0da210315a1aacb213edbb
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-05-10 20:37:28 -0300

    final de clase

---
+++ clase-5/for.cs
@@ -0,0 +1,5 @@
+for (int cantCara = 0; cantCara <= 12; cantCara++)//-- Lo haria infinito
+{
+    Console.WriteLine("Mi bolsita tiene " + cantCara + "caramelos");
+    //RECORDAR!!! el paso lo hace al finalizar la ejecucion
+}
